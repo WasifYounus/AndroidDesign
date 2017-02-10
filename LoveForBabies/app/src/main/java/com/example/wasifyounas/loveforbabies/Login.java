e241c3d7cb5d178a5477662a03223470003651c1
WasifYounus <wasifys@gmail.com>
2017-02-10 16:51:26 +0500

    facebook integration

--- LoveForBabies/app/src/main/java/com/example/wasifyounas/loveforbabies/Login.java
+++ LoveForBabies/app/src/main/java/com/example/wasifyounas/loveforbabies/Login.java
@@ -6,6 +6,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import com.facebook.FacebookSdk;
+
 
 public class Login extends AppCompatActivity {
 
@@ -13,17 +15,10 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+
+
     }
 
 }
