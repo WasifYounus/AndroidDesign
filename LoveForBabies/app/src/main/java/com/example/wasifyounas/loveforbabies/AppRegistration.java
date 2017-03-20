d4a59bec696b55280bbd904db2733fa7b16c5c8d
WasifYounus <wasifys@gmail.com>
2017-03-20 17:26:21 +0500

    2017-03-20

---
+++ LoveForBabies/app/src/main/java/com/example/wasifyounas/loveforbabies/AppRegistration.java
@@ -0,0 +1,13 @@
+package com.example.wasifyounas.loveforbabies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AppRegistration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_registration);
+    }
+}
